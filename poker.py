e5f3a9c1e9676ea1e442bd9e4d8da2857c1a2e36
valoche3 <120192549+valoche3@users.noreply.github.com>
2023-01-06 14:15:13 +0100

    Update poker.py

--- poker.py
+++ poker.py
@@ -1,12 +1,13 @@
 from src.miseEnPlace import Partie
 from src.combinaisongagnante import détermination_du_vainqueur
-import src.tour_de_jeu
+from src.tour_de_jeu import tour_de_jeu
 
 def main():
     partie = Partie()
     print("Début du jeu")
     print(f"Les mains des joueurs sont : {partie.mains}")
     print(f"Les cartes du milieu sont : {partie.cartes_du_milieu}")
+    tour_de_jeu(
     détermination_du_vainqueur(partie.mains, partie.cartes_du_milieu)
     print("Fin du jeu")
 
