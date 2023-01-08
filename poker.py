9c709f0466a7fde867723cef0282988f8a33b27c
Aurélie Vignault <vignaurelie0303@gmail.com>
2023-01-08 17:39:35 +0100

    finalisé 8 janvier

--- poker.py
+++ poker.py
@@ -1,14 +1,27 @@
 from src.miseEnPlace import Partie
 from src.miseEnPlace import sous_Partie
 from src.combinaisongagnante import détermination_du_vainqueur
+from src.pourmel import tour_de_jeu
+from src.pourmel import détermination_dealer
+from src.pourmel import détermination_premier_dealer
 
 def main():
     partie = Partie()
-    sous_partie = sous_Partie(partie.nombre_de_joueurs)
-    print("Début du jeu")
-    print(f"Les mains des joueurs sont : {sous_partie.mains}")
-    print(f"Les cartes du milieu sont : {sous_partie.cartes_du_milieu}")
-    détermination_du_vainqueur(sous_partie.mains, sous_partie.cartes_du_milieu)
-    print("Fin du jeu")
+    continuer = 'oui'
+    dealer = None
+    while continuer == 'oui' :
+        sous_partie = sous_Partie(partie.nombre_de_joueurs)
+        print("Début du jeu")
+        if dealer == None :
+            dealer = détermination_premier_dealer(partie.nombre_de_joueurs)
+        else:
+            dealer = détermination_dealer(dealer)
+        print(f"Les mains des joueurs sont : {sous_partie.mains}")
+        print(f"Les cartes du milieu sont : {sous_partie.cartes_du_milieu}")
+        present = tour_de_jeu(partie.nombre_de_joueurs, sous_partie.mises, partie.argent, sous_partie.mise_verif, dealer)
+        détermination_du_vainqueur(present, sous_partie.cartes_du_milieu, sous_partie.mains)
+        print("Fin du jeu")
+        continuer = input()
+        
 
 main()
